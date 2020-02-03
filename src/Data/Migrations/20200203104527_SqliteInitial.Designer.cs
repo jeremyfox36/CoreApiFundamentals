6a7e832df324da4a115db8d2d0e1725d327d0b17
Jeremy Fox <jeremy.fox@cps.scot>
2020-02-03 11:54:47 +0000

    Added automapper and CampModel

--- src/Data/Migrations/20200203104527_SqliteInitial.Designer.cs
+++ src/Data/Migrations/20200203104527_SqliteInitial.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CoreCodeCamp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreCodeCamp.Migrations
 {
     [DbContext(typeof(CampContext))]
-    partial class CampContextModelSnapshot : ModelSnapshot
+    [Migration("20200203104527_SqliteInitial")]
+    partial class SqliteInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
